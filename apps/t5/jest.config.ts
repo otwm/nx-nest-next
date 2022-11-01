e1ff75409e69ead00106759e5e3bb1a30c33e54e
김동오/F&B팀(YA) <otwm@yanolja.com>
2022-11-01 17:50:54 +0900

    initial commit

---
+++ apps/t5/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 't5',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/t5',
+};
