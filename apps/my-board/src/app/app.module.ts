28fa0ce7ad075c427042f31758c5b2a7256fde61
김동오/F&B팀(YA) <otwm@yanolja.com>
2022-11-04 15:15:49 +0900

    feat: add page module and delete dummy

--- apps/my-board/src/app/app.module.ts
+++ apps/my-board/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MyBoardPagesModule} from '@nx-nest-next/my-board-pages'
 
 @Module({
-  imports: [],
+  imports: [MyBoardPagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
